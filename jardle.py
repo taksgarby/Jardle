876ede4df06dffee6d95d41d4d05dee67d63034c
Takako Onozuka <onozukatakako@gmail.com>
2023-05-13 17:24:58 +0100

    added constructors

---
+++ jardle.py
@@ -0,0 +1,4 @@
+class Jardle:
+
+    def __init__(self):
+        pass
--- play_jardle.py
+++ play_jardle.py
@@ -1,5 +1,8 @@
+from jardle import Jardle
+
 def main():
     print("Hello Jardle!")
+    jardle = Jardle()
 
 if __name__ == "__main__":
     main()
