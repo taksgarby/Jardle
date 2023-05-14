10575fd8b096229194258b9bb041da6b012bf25b
Takako Onozuka <onozukatakako@gmail.com>
2023-05-14 11:27:19 +0100

    added guessing input

--- jardle.py
+++ jardle.py
@@ -1,4 +1,10 @@
 class Jardle:
 
-    def __init__(self):
+    MAX_ATTEMPTS = 6
+    WORD_LENGTH = 4
+
+    def __init__(self, secret:str):
+        self.secret: str = secret
+        self.attempts = []
         pass
+        
--- play_jardle.py
+++ play_jardle.py
@@ -2,7 +2,14 @@ from jardle import Jardle
 
 def main():
     print("Hello Jardle!")
-    jardle = Jardle()
+    jardle = Jardle("えんぴつ")
+    
+    while True: 
+        x = input("Your guess: ")
+        if x == jardle.secret:
+            print("Well done!")
+            break
+        print("Not quite.")
 
 if __name__ == "__main__":
     main()
