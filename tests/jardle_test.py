504d22f89b451310b36b6125284b4a881b0482b9
Takako Onozuka <onozukatakako@gmail.com>
2023-06-07 17:03:59 +0100

    added more tests

--- tests/jardle_test.py
+++ tests/jardle_test.py
@@ -8,3 +8,11 @@ class TestJardle(unittest.TestCase):
 
     def test_game_has_secret_word(self):
         self.assertEqual("えんぴつ", self.jardle.secret)
+    
+    def test_user_can_make_a_guess(self):
+        self.jardle.attempt("えんそく")
+        self.assertEqual(1, len(self.jardle.attempts))
+    
+    def test_game_has_remaining_attempts(self):
+        self.jardle.attempt("えんそく")
+        self.assertEqual(5, self.jardle.remaining_attempts)
